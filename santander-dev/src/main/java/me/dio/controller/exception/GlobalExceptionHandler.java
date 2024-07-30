1a9fd4a38450ae4a72286aa6cd4d2f7c0a59a6a1
virtoo <victorhfidelis@gmail.com>
2024-07-29 21:10:26 -0300

    first commit

---
+++ santander-dev/src/main/java/me/dio/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package me.dio.controller.exception;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(IllegalAccessException.class)
+    public ResponseEntity<String> handle(IllegalAccessException businessException){
+        return new ResponseEntity<>(businessException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(IllegalAccessException.class)
+    public ResponseEntity<String> handle(NoSuchElementException notFoungException){
+        return new ResponseEntity<>("Resource ID not found", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<String> handle(Throwable unexpectedException){
+        var message = "Unexpected server error, see the logs";
+        logger.error(message,unexpectedException);
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
---
+++ santander-dev/src/main/java/me/dio/domain/model/Feature.java
@@ -0,0 +1,7 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_features")
+public class Feature extends BaseItem{
+}
